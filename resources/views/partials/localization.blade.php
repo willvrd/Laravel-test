205001ea38a4d72fde52d50224d9470c4226d65a
William Verde <williamverde10@gmail.com>
2020-06-12 16:53:45 -0400

    Added: set locale pages

--- resources/views/partials/localization.blade.php
+++ resources/views/partials/localization.blade.php
@@ -1,6 +1,8 @@
+@php $locale = LaravelLocalization::setLocale() ?: App::getLocale(); @endphp
+
 <ul class="list-group list-group-horizontal">
     @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
-        <li class="list-group-item list-group-item-secondary">
+        <li class="list-group-item {{$locale==$localeCode?"list-group-item-primary":"list-group-item-secondary"}}">
             <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
                 {{ $properties['native'] }}
             </a>
